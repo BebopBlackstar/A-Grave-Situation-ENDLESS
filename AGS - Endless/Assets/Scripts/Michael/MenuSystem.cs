8a045ddd7671960c3a7f85baca7a6cad94bbcbb5
Michael Gackle <michael.gackle@ad.aie.edu>
2016-10-18 13:21:05 +1030

    Menu System with some other stuff including a title screen.

---
+++ AGS - Endless/Assets/Scripts/Michael/MenuSystem.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[SerializeField]
+public class playerStats
+{
+    public int saveSlots = 3;
+    //public Button[] UI_saveSlots;
+    float[] Save_money = {0, 0, 0};
+    int[] Save_Level = {0, 0, 0};
+}
+
+public class MenuSystem : MonoBehaviour {
+
+    public Scene scenes;
+    public int Checktest = 3;
+    int num_loadGame = 0;
+    
+    void Start()
+    { /*GetComponent<MenuSystem>().GetComponent<playerStats>().UI_saveSlots;*/ }
+
+    public void newGame() //load up the first level for training
+    {
+
+        SceneManager.LoadScene(1);
+    }
+
+    public void resume()
+    { }
+
+    public void saveGame()
+    { }
+
+    public void loadGame()
+    {    }
+
+    public void leaderboards()
+    {    }
+
+    public void options()
+    {    }
+
+    public void credits()
+    {    }
+
+    public void quit()
+    {    }
+}
