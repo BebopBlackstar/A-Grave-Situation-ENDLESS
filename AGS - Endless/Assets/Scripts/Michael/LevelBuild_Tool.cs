95a044593b4e4637a0ced2bef919d4263c0787bf
Talscar-PC\Talscar <talscartalcom@gmail.com>
2016-10-19 23:54:08 +1030

    Trying to load up the tools to the push point in time.

---
+++ AGS - Endless/Assets/Scripts/Michael/LevelBuild_Tool.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LevelBuild_Tool : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
